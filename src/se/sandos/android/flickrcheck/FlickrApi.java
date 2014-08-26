7c48f54f27f48e7482ad93e8d89e0de59f28322e
John Bäckstrand <sopues@gmail.com>
2014-08-26 14:14:46 +0200

    Organize imports

--- src/se/sandos/android/flickrcheck/FlickrApi.java
+++ src/se/sandos/android/flickrcheck/FlickrApi.java
@@ -3,18 +3,10 @@ package se.sandos.android.flickrcheck;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Map;
 
-import com.fasterxml.jackson.jr.ob.JSON;
-
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.SharedPreferences.Editor;
-import android.net.Uri;
-import android.util.Log;
 import oauth.signpost.OAuth;
 import oauth.signpost.OAuthConsumer;
 import oauth.signpost.OAuthProvider;
@@ -27,12 +19,18 @@ import oauth.signpost.exception.OAuthNotAuthorizedException;
 import oauth.signpost.http.HttpParameters;
 import se.sandos.android.flickrcheck.json.PhotoInfo;
 import se.sandos.android.flickrcheck.json.PhotoSearch;
-import se.sandos.android.flickrcheck.json.PhotoSetList;
-import se.sandos.android.flickrcheck.json.Sizes;
 import se.sandos.android.flickrcheck.json.PhotoSearch.Photos;
+import se.sandos.android.flickrcheck.json.PhotoSetList;
 import se.sandos.android.flickrcheck.json.PhotoSetList.PhotoSets;
-import se.sandos.android.flickrcheck.json.PhotoSetList.PhotoSets.PhotoSet;
+import se.sandos.android.flickrcheck.json.Sizes;
 import se.sandos.android.flickrcheck.json.Sizes.Size.SizeLine;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.net.Uri;
+import android.util.Log;
+
+import com.fasterxml.jackson.jr.ob.JSON;
 
 public class FlickrApi {
 	private static final String API_KEY = "a03761049a0bfcd06fecfc3df77d553e";
